fd1396aeef797e208571868061a2dcd3d0a86d2f
khosbilegt <khosookoo@gmail.com>
2023-05-09 23:08:20 +0800

    added line, spline and pie

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Navbar from './components/Navbar';
 import CompareBarImage from "./assets/images/compare_bar.png";
 import StackedBarImage from "./assets/images/stacked_bar.png";
 import PieImage from "./assets/images/pie.png";
+import DonutImage from "./assets/images/donut.png";
+import LineImage from "./assets/images/line.png";
+import SplineImage from "./assets/images/spline.png";
 
 function App() {
   useEffect(() => {
@@ -18,6 +21,9 @@ function App() {
         <ChartLink img={CompareBarImage} label="Bar Chart (Compare)" link="/compare-bar"/>
         <ChartLink img={StackedBarImage} label="Bar Chart (Stacked)" link="/stacked-bar"/>
         <ChartLink img={PieImage} label="Pie Chart" link="/pie"/>
+        <ChartLink img={DonutImage} label="Donut Chart" link="/donut"/>
+        <ChartLink img={LineImage} label="Line Chart" link="/line"/>
+        <ChartLink img={SplineImage} label="Spline Chart" link="/spline"/>
       </ul>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 import CompareBarChart from './charts/CompareBarChart';
 import PieChart from './charts/PieChart';
 import StackedBarChart from './charts/StackedBarChart';
+import DonutChart from './charts/DonutChart';
+import LineChart from './charts/LineChart';
+import SplineChart from './charts/SplineChart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,6 +20,9 @@ root.render(
           <Route path="compare-bar" element={<CompareBarChart />} />
           <Route path="stacked-bar" element={<StackedBarChart />} />
           <Route path="pie" element={<PieChart />} />
+          <Route path="donut" element={<DonutChart />} />
+          <Route path="line" element={<LineChart />} />
+          <Route path="spline" element={<SplineChart />} />
         </Route>
       </Routes>
     </BrowserRouter>
